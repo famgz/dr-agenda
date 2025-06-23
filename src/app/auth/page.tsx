3e3eb8e933da4b04d60226e2d811abdfe2cfb413
famgz <famgz@proton.me>
2025-06-23 15:26:57 -0300

    feat: implement proper redirects

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,19 @@
 import LoginForm from '@/app/auth/_components/login-form';
 import RegisterForm from '@/app/auth/_components/register-form';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function AuthPage() {
+export default async function AuthPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session?.user) {
+    redirect('/dashboard');
+  }
+
   return (
     <div className="flex-center h-screen w-screen">
       <Tabs defaultValue="login" className="w-[400px]">
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,23 @@
-export default function DashboardPage() {
-  return <div>Dashboard</div>;
+import SignOutButton from '@/components/sign-out-button';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect('/auth');
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+      <SignOutButton />
+    </div>
+  );
 }
