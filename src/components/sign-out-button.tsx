0fc0cfe823a40afe022b17c3575dce80b8491e95
famgz <famgz@proton.me>
2025-06-23 19:24:30 -0300

    feat: ensure sign out button is disabled during signout

--- src/components/sign-out-button.tsx
+++ src/components/sign-out-button.tsx
@@ -3,17 +3,22 @@
 import { Button } from '@/components/ui/button';
 import { authClient } from '@/lib/auth-client';
 import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
 
 export default function SignOutButton() {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  async function handleSignOut() {
+    startTransition(async () => {
+      await authClient.signOut({
+        fetchOptions: { onSuccess: () => router.push('/auth') },
+      });
+    });
+  }
+
   return (
-    <Button
-      onClick={() =>
-        authClient.signOut({
-          fetchOptions: { onSuccess: () => router.push('/auth') },
-        })
-      }
-    >
+    <Button onClick={handleSignOut} disabled={isPending}>
       Sair
     </Button>
   );
